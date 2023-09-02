539b60d5b4c1afd596b9a086c1e54f4e1ef949e9
Marco Stradolini <143885514+MAZStradolini@users.noreply.github.com>
2023-09-02 15:29:25 -0300

    Update DictionaryRT.py

--- Dados/DictionaryRT.py
+++ Dados/DictionaryRT.py
@@ -1,1 +1,28 @@
+Services = ['Consultoria/Desenvolvimento - Jira',
+            'Consultoria/Desenvolvimento',
+            'Administração e Manutenção de Redes Locais',
+            'Disponibilização de Servidor Computacional',
+            'Disponibilização de Servidor de Arquivos',
+            'Hospedagem de Aplicação e Armazenamento de Dados - Sistemas',
+            'Gestão e operação da Rede Digital de Telefonia Municipal (RDTM)', 
+            'Suporte técnico a evento sazonal', 
+            'Consultoria de Infraestrutura', 
+            'Outro (informar):', 
+            'Suporte local a hardware e software - estação com garantia',
+            'Suporte local a hardware e software - estação sem garantia',
+            'Suporte a impressoras',
+            'Administração de Correio Eletrônico (e-mail)',
+            'Administração e Manutenção de Câmeras de Videomonitoramento - indoor',
+            'Administração e Manutenção de Câmeras de Videomonitoramento - Outdoor',
+            'Administração e Manutenção de Rádio Wi-Fi - indoor',
+            'Administração e Manutenção de Rádio Wi-Fi - outdoor',
+            'Suporte e manutenção de Terminal de Radiocomunicação Digital',
+            'Administração e Manutenção da Rede de Radiocomunicação Digital',
+            'Gestão da Rede Infovia',
+            'Análise e Desenvolvimento de sistemas de informação']
 
+Setores = ['GP', 'PGM', 'SMAMUS', 'SMAP', 'SMC', 'SMDET', 'SMDS', 'SMED', 'SMGOV', 'SMOI', 'SMP', 'SMS - Geral', 'SMS - AB', 'SMS - CGVS', 'SMSEG', 'SMS - HMIPV', 'SMS - HPS', 'SMS - PA', 'SMS - SAMU', 'SMS - SM', 'SMSURB', 'SMTC', 'SMPAE', 'DMLU', 'EPTC', 'FASC', 'PREVIMPA', 'CARRIS', 'DEMHAB', 'DMAE', 'SMF', 'IMESF', 'SMS-CR(Estado)', 'SMHARF', 'SMMU', 'SMELJ']
+
+mes_competencia = ['JANEIRO', 'FEVEREIRO', 'MARÇO', 'ABRIL', 'MAIO', 'JUNHO', 'JULHO', 'AGOSTO', 'SETEMBRO', 'OUTUBRO', 'NOVEMBRO', 'DEZEMBRO']
+
+ano_competencia = ['2020', '2021', '2022', '2023', '2024', '2025']
